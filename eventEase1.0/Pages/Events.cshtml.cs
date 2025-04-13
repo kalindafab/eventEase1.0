3cdf13297d12eb5c7bec3d99e1f9bac5b9974c14
nh24856 <heritiernkinzingabo@gmail.com>
2025-04-13 17:28:02 +0200

    Add event update, event display for the event creator. Also added the view of events to normal user(user), pending to add nearby display functionality

--- eventEase1.0/Pages/Events.cshtml.cs
+++ eventEase1.0/Pages/Events.cshtml.cs
@@ -1,12 +1,64 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace eventEase1._0.Pages
 {
     public class EventsModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+
+        public EventsModel(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
+
+        public List<EventItem> EventItem { get; set; } = new List<EventItem>();
+
+        public async Task OnGetAsync()
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = @"SELECT Id, Name, Price, Tickets, EventDate, Location, ImagePath FROM Events";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var evt = new EventItem
+                            {
+                                Id = reader.GetGuid(0),
+                                Name = reader.GetString(1),
+                                Price = reader.GetDecimal(2),
+                                Tickets = reader.GetInt32(3),
+                                EventDate = reader.GetDateTime(4),
+                                Location = reader.GetString(5),
+                                ImagePath = reader.IsDBNull(6) ? "default-event.jpg" : reader.GetString(6),
+                            };
+                            EventItem.Add(evt);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class EventItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Tickets { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+        public string ImagePath { get; set; }
     }
 }
--- eventEase1.0/Pages/myEvents.cshtml.cs
+++ eventEase1.0/Pages/myEvents.cshtml.cs
@@ -1,65 +1,77 @@
-using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
-
-
-
-public class Event
-{
-    public int EventId { get; set; }
-    public string EventTitle { get; set; }
-    public string EventDescription { get; set; }
-    public DateTime EventDate { get; set; }
-    public string EventLocation { get; set; }
-    public string EventImage { get; set; } // URL or file path for the image
-}
-
-
-
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Security.Claims;
 
 namespace eventEase1._0.Pages
 {
     public class myEventsModel : PageModel
     {
-        private void FetchManagerEvents(string managerId)
+        private readonly IConfiguration _configuration;
+
+        public myEventsModel(IConfiguration configuration)
         {
-            try
+            _configuration = configuration;
+        }
+
+        public List<EventItem> UserEvents { get; set; } = new List<EventItem>();
+
+        public class EventItem
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Tickets { get; set; }
+            public DateTime EventDate { get; set; }
+            public string Location { get; set; }
+            public string ImagePath { get; set; }
+            public int SoldTickets => TotalTickets - Tickets;
+            public int TotalTickets { get; set; }  // Original Ticket count
+        }
+
+        public void OnGet()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
             {
-                string connectionString = _configuration.GetConnectionString("DefaultConnection");
+                TempData["ErrorMessage"] = "User not logged in.";
+                return;
+            }
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"SELECT Id, Name, Price, Tickets, EventDate, Location, ImagePath, Tickets as TotalTickets
+                                 FROM Events WHERE UserId = @UserId";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    connection.Open();
-                    string query = "SELECT EventId, EventTitle, EventDate, EventLocation FROM Events WHERE ManagerId = @ManagerId";
-
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    using (var reader = command.ExecuteReader())
                     {
-                        command.Parameters.AddWithValue("@ManagerId", managerId);
-
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        while (reader.Read())
                         {
-                            myEventsModel = new List<Event>();
-                            while (reader.Read())
+                            var evt = new EventItem
                             {
-                                ManagerEvents.Add(new Event
-                                {
-                                    EventId = (int)reader["EventId"],
-                                    EventTitle = reader["EventTitle"].ToString(),
-                                    EventDate = (DateTime)reader["EventDate"],
-                                    EventLocation = reader["EventLocation"].ToString()
-                                });
-                            }
+                                Id = reader.GetGuid(0),
+                                Name = reader.GetString(1),
+                                Price = reader.GetDecimal(2),
+                                Tickets = reader.GetInt32(3),
+                                EventDate = reader.GetDateTime(4),
+                                Location = reader.GetString(5),
+                                ImagePath = reader.IsDBNull(6) ? "default-event.jpg" : reader.GetString(6),
+                                TotalTickets = reader.GetInt32(7)  // Assuming the original tickets were stored here
+                            };
+                            UserEvents.Add(evt);
                         }
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                // Handle any exceptions here, like database connectivity issues.
-            }
         }
     }
-
-
 }
